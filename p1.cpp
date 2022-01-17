a426f2d56d6bcacc04202cc6c2bda61559b2c6b8
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-01-17 16:03:31 +0100

    Prime checker is done, working on how to make threading work.

--- p1.cpp
+++ p1.cpp
@@ -4,8 +4,11 @@
 #include <mutex>
 #include <bits/stdc++.h>
 
+
 using namespace std;
-vector<vector<int>> matrixArray;
+vector<int> result;
+vector<thread> threads;
+mutex result_mutex;
 
 bool primeCalc(int num) {
     if(num<=1) return false;
@@ -16,6 +19,49 @@ bool primeCalc(int num) {
     return true;
 }
 
-int main() {
+void threadWord(int start, int end) {
+}
 
+/*
+void threadWork(int start, int end) {
+    thread t1([&start, &end] {
+        for (int i = start; i < end/2; ++i) {
+            unique_lock<mutex> lock(result_mutex);
+            if(primeCalc(true)) {
+                result.emplace_back(i);
+            }
+        }
+    });
+
+    thread t2([&end]{
+        for (int i = end/2; i <= end; ++i) {
+            unique_lock<mutex> lock(result_mutex);
+            if(primeCalc(true)) {
+                result.emplace_back(i);
+            }
+        }
+    });
+    t1.join();
+    t2.join();
+}
+
+void print(vector<int> const &input) {
+    for (int i : input) {
+        cout << i << ' ';
+    }
+}
+*/
+
+int main() {
+    int start;
+    int end;
+
+    cout << "Select start of interval: ";
+    cin >> start;
+    cout << "Select end of interval: ";
+    cin >> end;
+
+    threadWork(start, end);
+
+    print(result);
 }
