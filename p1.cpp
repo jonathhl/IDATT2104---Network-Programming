0d2f466e73e59e59fa1cbf81ecff8267cbaaf55c
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-01-17 21:27:37 +0100

    Trying execution::par instead.

--- p1.cpp
+++ p1.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <mutex>
+// #include <thread>
 #include <bits/stdc++.h>
-
+#include <execution>
+#include <algorithm>
 
 using namespace std;
 vector<int> result;
-vector<thread> threads;
-mutex result_mutex;
+// vector<thread> threads;
 
 bool primeCalc(int num) {
     if(num<=1) return false;
@@ -19,8 +18,35 @@ bool primeCalc(int num) {
     return true;
 }
 
-void threadWord(int start, int end) {
+void threadWork(int start, int end) {
+    vector<int> list;
+    for (int i = start; i <= end; ++i) {
+        list.emplace_back(i);
+    }
+
+    transform(execution::par, list.begin(), list.end(), [](int num) {
+        if(primeCalc(num)) {
+            return result.emplace_back(primeCalc(num));
+        }
+    });
 }
+
+/*
+void threadWork(int start, int end) {
+    for (int thread_number = 0; thread_number < 2; thread_number++) {
+        threads.emplace_back([thread_number, &start, &end]{
+            for (int i = thread_number * 2; i <= thread_number * 2 + 1; i++) {
+                if(primeCalc(i))
+                    result[i] = primeCalc(i);
+            }
+        });
+    }
+
+    for(auto &t : threads) {
+        t.join();
+    }
+}
+*/
 
 /*
 void threadWork(int start, int end) {
@@ -44,13 +70,13 @@ void threadWork(int start, int end) {
     t1.join();
     t2.join();
 }
+ */
 
 void print(vector<int> const &input) {
     for (int i : input) {
         cout << i << ' ';
     }
 }
-*/
 
 int main() {
     int start;
