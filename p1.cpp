2f2a30592119ad7c63a2fdd680e196f10bab14ad
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-01-20 19:04:17 +0100

    Working on a new way to separate thread work.

--- p1.cpp
+++ p1.cpp
@@ -7,24 +7,47 @@ vector<int> result;
 vector<thread> threads;
 
 bool primeCalc(int num) {
-    if(num<=1) return false;
-    if(num == 2) return true;
-    for (int i = 2; i <= sqrt(num); ++i) {
-        if(num%i == 0) return false;
-    }
+    if(num <= 1) return false;
+    if(num <= 3) return true;
+    
+    if(num % 2 == 0 || num % 3 == 0)
+        return false;
+
+
+    for (int i = 5; i * i <= num; i = i + 6)
+        if(num % i == 0 || num % (i + 2) == 0)
+            return false;
+
     return true;
 }
 
 // TODO: fix threading, threads each does the interval throughout once. Not optimal.
 void threadWork(int start, int end) {
-    for (int thread_number = 0; thread_number < 2; thread_number++) {
-        threads.emplace_back([thread_number, &start, &end]{
+
+    for (int thread_number = 0; thread_number < 5; thread_number++) {
+        threads.emplace_back([&thread_number, &start, &end]{
             for (int i = start; i <= end; i++) {
                 if(primeCalc(i))
                     result.emplace_back(i);
             }
         });
     }
+
+    /*
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back([&i, &temp] {
+           bool task_done = false;
+           int nr = i;
+           while(!task_done) {
+               for (int j = temp.size(); j >= temp.size()/5; --j) {
+                   if(primeCalc(j)) {
+                       result.emplace_back(j);
+                   }
+               }
+           }
+        });
+    }
+     */
 
     for(auto &t : threads) {
         t.join();
@@ -40,11 +63,18 @@ void print(vector<int> const &input) {
 int main() {
     int start;
     int end;
+    vector<int> temp;
 
-    cout << "Select start of interval:";
+    cout << "Start of interval:";
     cin >> start;
-    cout << "Select end of interval:";
+    cout << "End of interval:";
     cin >> end;
+
+    /*
+    for (int i = start; i <= end; ++i) {
+        temp.emplace_back(i);
+    }
+     */
 
     threadWork(start, end);
 
